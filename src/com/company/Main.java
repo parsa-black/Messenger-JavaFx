e28693761f23d12d4ff19da30dd0058b3665cd7a
Parsa Sajedi <parsagorgzan1225@gmail.com>
2021-12-27 18:58:54 +0330

    in Main.java change ("good" to "bad")

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage stage) throws  Exception {
+
+        Button button = new Button("this is good");
+        AnchorPane pane = new AnchorPane();
+        pane.getChildren().addAll(button);
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
