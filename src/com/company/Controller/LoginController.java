5af2ab5ecf7a71f7c836d2d960b3bee2f6dd5485
Parsa Sajedi <parsagorgzan1225@gmail.com>
2021-12-31 12:26:20 +0330

    change in login controller

--- src/com/company/Controller/LoginController.java
+++ src/com/company/Controller/LoginController.java
@@ -1,6 +1,44 @@
 package com.company.Controller;
 
 
-public class LoginController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController  implements Initializable {
+
+    @FXML
+    Button button_singup;
+
+    @FXML
+    Button button_login;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        button_singup.setOnAction(e -> {
+            try {
+                openRegisterPage();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+    }
+
+    private void openRegisterPage() throws IOException {
+        HBox root = FXMLLoader.load(this.getClass().getResource("../View/RegisterView.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("register user");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,28 @@ package com.company;
 import com.company.Controller.DBUser;
 import com.company.Model.User;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class Main  {
+public class Main extends Application{
 
-    public static void main(String[] args) throws SQLException {
-
-        System.out.println("It's WOrk!");
+    public static void main(String[] args) {
+        launch(args);
     }
 
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        HBox root = FXMLLoader.load(this.getClass().getResource("/com/company/View/LoginView.fxml"));
+
+        stage.setTitle("Messenger");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
