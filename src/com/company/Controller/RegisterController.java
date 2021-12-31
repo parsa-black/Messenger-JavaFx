a0de31d7da2c3fa466ef0e6be3d147b259711d30
Parsa Sajedi <parsagorgzan1225@gmail.com>
2021-12-31 13:58:29 +0330

    change Rigester in login controller

--- src/com/company/Controller/RegisterController.java
+++ src/com/company/Controller/RegisterController.java
@@ -1,5 +1,14 @@
 package com.company.Controller;
 
-public class RegisterController {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController  implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
 }
